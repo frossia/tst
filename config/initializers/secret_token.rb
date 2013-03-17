36ac671cdd2077ddb355fdc2844ef5f71dc9534e
frossia <frossiacsb@gmail.com>
2013-03-17 17:32:09 +0400

    Oter Files of Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tst::Application.config.secret_token = '70a87958053fcf2c175025885386aabdb2bc7a1559c11753fe12f779eb86501ab947a3816f9c9cec537dc8065c3740c70f7867076ed232d10796cd6e60b8f81e'
